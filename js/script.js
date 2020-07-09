ca2978f209b8af0a6658483863b01465fa46da73
Alberto Zordan <zorberto@gmail.com>
2020-07-09 15:42:46 +0200

    Dice game done!

--- js/script.js
+++ js/script.js
@@ -1,23 +1,62 @@
-var email =prompt('Inserisci il tuo indirizzo email');
-var indirizzi =['tizio@hotmail.com','caio@gmail.com','sempronio@yahoo.it','pinco@outlook.com',
-'pallino@mail.com','toni@virgilio.it','bepi@email.it','areo@libero.it'];
+// var email =prompt('Inserisci il tuo indirizzo email');
+// var indirizzi =['tizio@hotmail.com','caio@gmail.com','sempronio@yahoo.it','pinco@outlook.com',
+// 'pallino@mail.com','toni@virgilio.it','bepi@email.it','areo@libero.it'];
+//
+// // var stop = false;
+// // var bgc;
+// // var message;
+// //
+// // for (var i = 0; i < indirizzi.length; i++) {
+// //     if (indirizzi[i] == email) {
+// //         stop = true;}
+// // }
+// //
+// // if (stop) {
+// //     message = 'Benvenuto '+ email.substring(0, email.indexOf("@"))+'!';
+// //     bgc = "lightgreen";
+// // }   else {
+// //     message = 'Ci dispiace, il tuo indirizzo email non è presente nel nostro database.';
+// //     bgc = "#e60000";
+// // }
+// //
+// // document.getElementById('esito').innerHTML= message;
+// // document.getElementById('esito').style.backgroundColor = bgc;
+//
+// // OPPURE
+//
+// var stop = 'Ci dispiace, il tuo indirizzo email non è presente nel nostro database.';
+// var bgc = "#e60000";
+//
+// for (var i = 0; i < indirizzi.length; i++) {
+//     if (indirizzi[i] == email) {
+//         stop = 'Benvenuto '+ email.substring(0, email.indexOf("@"))+'!';
+//         bgc = "lightgreen";
+//     }
+// }
+//
+// document.getElementById('esito').innerHTML= stop;
+// document.getElementById('esito').style.backgroundColor = bgc;
 
-var stop = false;
-var bgc;
-var message;
+// ESERCIZIO DADI
+// Faccio estrarre un numero casuale 1 <= x <= 6 sia all'utente che al computer
+var utente = Math.floor(Math.random()*6) + 1;
+var computer = Math.floor(Math.random()*6) + 1;
+// Inizializzo una variabile "risultato" e una variabile "background color" ai valori di parità tra i due giocatori
+var risultato = 'Non vince nessuno, tiè!';
+var bgc = "brown";
 
-for (var i = 0; i < indirizzi.length; i++) {
-    if (indirizzi[i] == email) {
-        stop = true;}
-}
-
-if (stop) {
-    message = 'Benvenuto '+ email.substring(0, email.indexOf("@"))+'!';
+// Istruzione condizionale per decretare il vincitore, se ce n'è uno...
+if (utente > computer) {
+    risultato = 'Bravo, hai vinto!';
     bgc = "lightgreen";
-}   else {
-    message = 'Ci dispiace, il tuo indirizzo email non è presente nel nostro database.';
+}   else if (utente < computer) {
+    risultato = 'Azz, il computer ti ha battuto!';
     bgc = "#e60000";
 }
 
-document.getElementById('esito').innerHTML= message;
+// Stampa sul documento del risultato, con relativo colore di background
+document.getElementById('esito').innerHTML= risultato;
 document.getElementById('esito').style.backgroundColor = bgc;
+
+console.log(utente);
+console.log(computer);
